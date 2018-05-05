228b3aae00442a0b74f8c640ea5bffaad0f6d7b6
Jack <jack9966qk@gmail.com>
2018-05-05 11:00:23 +1000

    added StateSync to act as a separate thread for networking, to be implemented

---
+++ Assets/Script/StateSync.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class StateSync {
+	public static Thread Thread = new Thread(mainLoop);
+	static GlobalState serverState;
+
+	public static GlobalState GetServerState() {
+		lock (serverState) {
+			return serverState;
+		}
+	}
+
+	public static void SetLocalState(int playerId, LocalState s) {
+		lock (serverState) {
+			serverState.PutLocalState(playerId, s);
+		}
+	}
+
+	static void mainLoop() {
+		while (true) {
+			// receive state from server
+			// ...
+			
+			// send local state to server
+			// ...
+		}
+	}
+}
