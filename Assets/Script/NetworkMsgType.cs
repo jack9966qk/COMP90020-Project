039a2a950a37985642653f0eac63e02de999e09c
Jack <jack9966qk@gmail.com>
2018-05-12 16:32:05 +1000

    more playerID related work

--- Assets/Script/NetworkMsgType.cs
+++ Assets/Script/NetworkMsgType.cs
@@ -1,4 +1,5 @@
 public class NetworkMsgType {
     public const short NewGlobalState = 1000;
     public const short StateChangeSubmission = 1001;
+    public const short AssignPlayerId = 1002;
 }
--- Assets/Script/ServerLogic.cs
+++ Assets/Script/ServerLogic.cs
@@ -1,14 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ServerLogic : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // read from ServerNetwork
-		// manage server side player and bullet objects
+	void ApplyStateChange(Dictionary<int, StateChange> stateChanges) {
+		// TODO init/destroy objects
+		// ...
+		// TODO update each object
 	}
 }
