0bb62bbae2df0b097e6989429cc650d363bbc40f
Jack <jack9966qk@gmail.com>
2018-05-12 16:57:46 +1000

    start server side logic

--- Assets/Script/ServerLogic.cs
+++ Assets/Script/ServerLogic.cs
@@ -2,9 +2,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ServerLogic : MonoBehaviour {
-	void ApplyStateChange(Dictionary<int, StateChange> stateChanges) {
-		// TODO init/destroy objects
-		// ...
-		// TODO update each object
+	GlobalState GlobalState;
+	public void ApplyStateChange(Dictionary<int, StateChange> stateChanges) {
+		foreach (var playerId in stateChanges.Keys) {
+			var change = stateChanges[playerId];
+			// add bullets
+			foreach (BulletState bulletState in change.BulletsCreated) {
+				// TODO init the bullets
+			}
+			// update player position
+			var playerState = GlobalState.GetLocalState(playerId).PlayerState;
+			playerState.Position = change.NewPosition;
+		}
+	}
+
+	public void OnCollision(int bulletId, int playerId) {
+		// apply damage to player
+		// var damage = Constants.BulletDamage;
+		// GlobalState.GetLocalState(playerId).PlayerState.Damage(damage);
+		// remove bullet
+		GlobalState.RemoveBulletState(bulletId);
 	}
 }
