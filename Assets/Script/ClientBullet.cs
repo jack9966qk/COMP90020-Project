92832c751bb36335c2c7f83af2d9dfac16e2a242
ryuzakighost <ryuzakighost@gmail.com>
2018-05-14 11:41:10 +1000

    premerge

---
+++ Assets/Script/ClientBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientBullet : MonoBehaviour {
+    public BulletState State = new BulletState();
+    public StateManager StateManager;
+    int frame;
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (StateManager != null) {
+            var globalState = StateManager.GetApproxState();
+            if (!globalState.BulletStates.ContainsKey(State.BulletID)) {
+                Debug.Log("Destroy");
+                Destroy(this.gameObject);
+            }
+            // update position from state
+            this.transform.position = globalState.BulletStates[State.BulletID].Position;
+        }
+        if (State == null) return;
+    }
+}
