7fb84b8b9b7c7a1f092a1c9b96251c538e1c09db
Jack <jack9966qk@gmail.com>
2018-05-15 12:27:09 +1000

    add serialisation/deserialisation for `BulletID` in `BulletState`

--- Assets/Script/BulletState.cs
+++ Assets/Script/BulletState.cs
@@ -7,15 +7,17 @@ using UnityEngine.Networking;
 public class BulletState : MessageBase {
 	public Vector2 Position;
 	public Direction Direction;
-    public string BulletID;
+	public string BulletID;
 
-    public override void Serialize(NetworkWriter writer) {
+	public override void Serialize(NetworkWriter writer) {
 		writer.Write(Position);
 		DirectionIO.writeDirectionToBuffer(Direction, writer);
-    }
+		writer.Write(BulletID);
+	}
 
-    public override void Deserialize(NetworkReader reader) {
+	public override void Deserialize(NetworkReader reader) {
 		Position = reader.ReadVector2();
 		Direction = DirectionIO.readDirectionFromBuffer(reader);
-    }
+		BulletID = reader.ReadString();
+	}
 }
